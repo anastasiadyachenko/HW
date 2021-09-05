a53e72eac269fb88b15568d38b522f3065961976
anastasiadyachenko <anastasiadyachenko3371@gmail.com>
2021-09-05 18:14:33 +0300

    Bug fixed

---
+++ HomeWorks/HomeWork12.py
@@ -0,0 +1,8 @@
+def dictcreator (keyy, val):
+    W = {keyy: val}
+    print(W)
+    return
+
+a = input("Введите ключ словаря: ")
+b = input("Введите значение словаря: ")
+dictcreator(a, b)
--- HomeWorks/HomeWork8.py
+++
@@ -1,8 +0,0 @@
-def dictcreator (keyy, val):
-    W = {keyy: val}
-    print(W)
-    return
-
-a = input("Введите ключ словаря: ")
-b = input("Введите значение словаря: ")
-dictcreator(a, b)
