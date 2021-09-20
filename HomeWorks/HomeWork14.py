45cea5794f7964902de69244e403d32f292c0c8b
anastasiadyachenko <anastasiadyachenko3371@gmail.com>
2021-09-20 21:04:53 +0300

    homework 14 done
    Создана база данных в файле и таблица в ней в четыре столбца с первичным ключем

---
+++ HomeWorks/HomeWork14.py
@@ -0,0 +1,19 @@
+import sqlite3
+connection = sqlite3.connect('test.db')
+cursor = connection.cursor()
+sql = '''create table if not exists Person
+                    (ID integer primary key autoincrement,
+                    FirstName varchar(100),
+                    SecondName varchar(100),
+                    Age integer)
+                    '''
+cursor.execute(sql)
+sql = "insert into Person(FirstName, SecondName, Age) values('Smith', 'John', 25)"
+cursor.execute(sql)
+connection.commit()
+sql = 'select * from Person'
+cursor.execute(sql)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+connection.close()
